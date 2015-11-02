4319145323fad5f3b5a3969b477930c3dde091cc
Aaron Gibson <aaron.gibson@cascadehr.co.uk>
2015-11-02 15:52:25 +0000

    minor additions

---
+++ MonolithDS/MonolithDS.Domain/Paging/INavigationRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MonolithDS.Domain.Ebay;
+using MonolithDS.Domain.Ebay.ProductManagement;
+
+namespace MonolithDS.Domain.Paging
+{
+    public interface INavigationRepository
+    {
+        IEnumerable<MenuItem> GetMenuItems();
+        EbayListingViewModel CreatListingViewModel(string pricerange, int page); 
+    }
+}
---
+++ MonolithDS/MonolithDS.Domain/Paging/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+    
+namespace MonolithDS.Domain.Paging
+{
+    public class MenuItem
+    {
+        public string Label { get; set; }
+        public Guid MenuItemId { get; set; }
+        public Guid TopMenuItemId { get; set; }
+
+    }
+}
