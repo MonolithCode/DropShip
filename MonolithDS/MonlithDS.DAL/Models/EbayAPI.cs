a9f83b1e7d20eb34d2a4003d24886c9751ff17d3
Aaron Gibson <aaron.gibson@cascadehr.co.uk>
2015-12-03 14:59:05 +0000

    slight change

---
+++ MonolithDS/MonlithDS.DAL/Models/EbayAPI.cs
@@ -0,0 +1,14 @@
+﻿using EbayModule;
+using EbayModule.eBaySvc;
+using EbayModule.enums;
+
+namespace MonlithDS.DAL.Models
+{
+    public partial class EbayAPI
+    {
+        public EbayProperties ToDomainObject(Modes mode, SiteCodeType code)
+        {
+            return new EbayProperties(AppID, DevID, AuthCert, Token, Runame, SandBoxToken, mode, code);
+        }
+    }
+}
